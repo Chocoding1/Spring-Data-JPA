32595d503ae5229f332416bc5b908888c42c9e9d
chocoding <galmeagi2@naver.com>
2025-02-18 00:25:37 +0900

    fetch join을 위한 EntityGraph 사용

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package study.data_jpa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -120,4 +121,25 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    /**
+     * 페치 조인
+     */
+    // 단순 JPQL 사용
+    @Query("select m from Member m join fetch m.team")
+    List<Member> findMemberFetchJoinTeam();
+
+    // findAll() 오버라이드 후, @EntityGraph 사용
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    // JPQL + @EntityGraph 사용
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    // 메서드명 + @EntityGraph 사용
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
 }
