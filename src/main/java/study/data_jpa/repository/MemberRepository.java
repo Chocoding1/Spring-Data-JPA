a48d68052bc2f2c6a3cfa340da926c280f751870
chocoding <galmeagi2@naver.com>
2025-03-03 13:39:53 +0900

    JPA Hint

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package study.data_jpa.repository;
 
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
@@ -142,4 +140,10 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     // 메서드명 + @EntityGraph 사용
     @EntityGraph(attributePaths = {"team"})
     List<Member> findEntityGraphByUsername(@Param("username") String username);
+
+    /**
+     * JPA Hint
+     */
+    @QueryHints(@QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
 }
